06e724a22c905bca2241ea3a619cc7cc9b7a98bb
Sanuja Methmal <methmal66@yahoo.com>
2020-12-31 21:38:53 +0530

    allow every host

--- geo_tracker/settings.py
+++ geo_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fg(99ist8ys#f1ui)!9degic7(qsliiq!@%szs+kv3v0$y(&5b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
