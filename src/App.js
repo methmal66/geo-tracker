cf4942b6693481caf1024d907989147cab78adbb
Sanuja Methmal <methmal66@yahoo.com>
2021-01-01 08:05:05 +0530

    remove webpack libs and config. fully depend on react-scripts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Map from "./components/Map";
 import Loader from "./components/Loader";
 import { useDispatch } from "react-redux";
